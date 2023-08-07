0c9b3da6d7c5df803675ac797f038e6d2610f2b4
Yarden BenMoshe <yardenbm@gmail.com>
2023-08-07 12:55:52 +0300

    Changed home path to /

--- src/App.tsx
+++ src/App.tsx
@@ -28,11 +28,11 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
+        <Route exact path="/">
           <Home />
         </Route>
         <Route exact path="/">
-          <Redirect to="/home" />
+          <Redirect to="/" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
