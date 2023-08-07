c258abb7026aa98f8b0952315dec8eeee9d6c890
Yarden BenMoshe <yardenbm@gmail.com>
2023-08-07 12:44:49 +0300

    new index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
   plugins: [
     react(),
     legacy()
   ],
-  build: {
-    outDir: 'dist'
-  },
   test: {
     globals: true,
     environment: 'jsdom',
