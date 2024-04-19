ff0a434854017b844948e25aaba56ce4c8e74988
Yarden <97087905+yardenbm@users.noreply.github.com>
2024-04-19 18:00:32 +0300

    removed api key for security

--- src/api/gpt.ts
+++ src/api/gpt.ts
@@ -1,8 +1,8 @@
 
 import { Configuration, OpenAIApi } from 'openai';
 
-const organization = 'org-2CwskBzgGP5OnJE5rJP2GrIS';
-const apiKey = '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';
+const organization = '<ORG_HERE>';
+const apiKey = '<API_KEY_HERE>';
 
 const configuration = new Configuration({
     organization,
